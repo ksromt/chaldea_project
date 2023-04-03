08ab2909ed0adb9ebd76551006214e0b0cc1f1ea
Kirisaki <kirisakipro@gmail.com>
2023-04-03 22:46:59 +0800

    fix: Repeated registration error
    Fix the error of registering an existing account name and give a prompt

--- app.py
+++ app.py
@@ -20,16 +20,34 @@ def home():
     return render_template("index.html", username=username)
 
 
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         username = request.form["username"]
+#         password = request.form["password"]
+#         user = User(username=username, password=password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash("Registration successful!", "success")
+#         return redirect(url_for("home"))
+#     return render_template("register.html")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        user = User(username=username, password=password)
-        db.session.add(user)
-        db.session.commit()
-        flash("Registration successful!", "success")
-        return redirect(url_for("home"))
+
+        # Check if the username already exists in the database
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("This user name has already been registered", "danger")
+            return render_template("register.html")
+        else:
+            user = User(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+            # flash("Registration successful!", "success")
+            return redirect(url_for("home"))
     return render_template("register.html")
 
 
@@ -40,11 +58,11 @@ def login():
         password = request.form["password"]
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-            session['username'] = user.username  # 将用户名存储在会话变量中
-            flash('登录成功！', 'success')
+            session['username'] = user.username  # Store the username in a session variable
+            flash('login successful！', 'success')
             return redirect(url_for('home'))
         else:
-            flash('登录失败，请检查用户名或密码。', 'danger')
+            flash('Login failed, please check username or password.', 'danger')
     return render_template("login.html")
 
 
