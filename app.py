cb2f2080b7fab8fc57a248b73ff869bb8cf43c63
Kirisaki <kirisakipro@gmail.com>
2023-04-15 21:14:07 +0800

    Update

--- app.py
+++ app.py
@@ -13,24 +13,21 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('posts', lazy=True))
+
 
 @app.route("/")
 def home():
     username = session.get('username', None)
-    return render_template("index.html", username=username)
+    posts = Post.query.order_by(Post.id.desc()).all()
+    return render_template("index.html", username=username, posts=posts)
 
 
-# @app.route("/register", methods=["GET", "POST"])
-# def register():
-#     if request.method == "POST":
-#         username = request.form["username"]
-#         password = request.form["password"]
-#         user = User(username=username, password=password)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash("Registration successful!", "success")
-#         return redirect(url_for("home"))
-#     return render_template("register.html")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -64,6 +61,42 @@ def login():
         else:
             flash('Login failed, please check username or password.', 'danger')
     return render_template("login.html")
+
+@app.route("/create_post", methods=["GET", "POST"])
+def create_post():
+    if not session.get('username'):
+        flash('You must be logged in to create a post.', 'danger')
+        return redirect(url_for('login'))
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        author_id = User.query.filter_by(username=session['username']).first().id
+        post = Post(title=title, content=content, author_id=author_id)
+        db.session.add(post)
+        db.session.commit()
+        flash("Post created successfully!", "success")
+        return redirect(url_for("home"))
+    return render_template("create_post.html")
+
+@app.route("/edit_post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if session.get('username') != post.author.username:
+        flash("You can only edit your own posts.", "danger")
+        return redirect(url_for('home'))
+    if request.method == "POST":
+        post.title = request.form["title"]
+        post.content = request.form["content"]
+        db.session.commit()
+        flash("Post updated successfully!", "success")
+        return redirect(url_for("home"))
+    return render_template("edit_post.html", post=post)
+
+@app.route("/post/<int:post_id>")
+def view_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("view_post.html", post=post)
+
 
 
 if __name__ == "__main__":
